3e0bbbacd076238a6c4d2220b65e660a40ef8793
carineho <carinehokh@gmail.com>
2024-05-10 11:53:45 +0800

    Added API call to obtain meme image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Meme.css";
-import memesData from "../memesData";
 
 export default function Meme() {
 
@@ -10,8 +9,16 @@ export default function Meme() {
         img: "http://i.imgflip.com/1bij.jpg",
     })
 
+    const [allMemes, setAllMemes] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    })
+
     function getMemeImage() {
-        const memeArray = memesData.data.memes;
+        const memeArray = allMemes;
         const randomNumber = Math.floor(Math.random() * memeArray.length);
         const url = memeArray[randomNumber].url;
         setMeme(prevMeme => {
